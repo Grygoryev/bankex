99e543daf517bba944b5540137ce9e34e271b3f6
Nickel 13 <nikesushko@gmail.com>
2018-09-14 20:18:35 +0300

    edits

--- src/app/components/SettingsMain.js
+++ src/app/components/SettingsMain.js
@@ -5,9 +5,10 @@ const SettingsMain = () => {
         <main className="settings-main box">
             <h2 className="settings-main__title">Settings</h2>
             <p className="settings-main__text">
-                Two factor authentification is enabled via MFA app. You can't disable 2FA now. If you are having problem with Custody service, contact us.
+                Two factor authentification is enabled via MFA app. You can't disable 2FA now. If you are having problem with Custody service, contact us
+                <a href="#" className="mail-link">support@bankexcustody.com</a>
             </p>
-            <a href="#" className="mail-link">support@bankexcustody.com</a>
+            
         </main>
     )
 }
